d805c7f937b3562876a31635d67a4a4f48de782e
Josh Paul <joshua.paul@revelationglobal.com>
2014-10-01 16:37:52 -0700

    a;lskdjf

--- lib/heatmap/image.rb
+++ lib/heatmap/image.rb
@@ -11,7 +11,7 @@ module Heatmap
       bounds = Heatmap::Geometry.bounds(area, 100)
 
       # Creating a blank canvas
-      system("convert -size #{bounds.width}x#{bounds.height} pattern:gray10 #{file.path}")
+      system("convert -size #{bounds.width}x#{bounds.height} #{file.path}")
 
       # Drawing each area
       compose = ["convert #{file.path}"]
