705d52b58b9f53d13f1b7a192419783b0828ac45
Josh Paul <joshua.paul@revelationglobal.com>
2014-10-01 17:00:33 -0700

    BRICKS

--- lib/heatmap/image.rb
+++ lib/heatmap/image.rb
@@ -11,7 +11,7 @@ module Heatmap
       bounds = Heatmap::Geometry.bounds(area, 100)
 
       # Creating a blank canvas
-      system("convert -size #{bounds.width}x#{bounds.height} pattern:none #{file.path}")
+      system("convert -size #{bounds.width}x#{bounds.height} pattern:bricks #{file.path}")
 
       # Drawing each area
       compose = ["convert #{file.path}"]
