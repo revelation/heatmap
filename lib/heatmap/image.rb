be6138c57a57aef8d848317bffc046f80abda522
Josh Paul <joshua.paul@revelationglobal.com>
2014-10-02 12:20:23 -0700

    IT WORKS!

--- lib/heatmap/image.rb
+++ lib/heatmap/image.rb
@@ -12,8 +12,8 @@ module Heatmap
 
       # Creating a blank canvas
       # system("convert -alpha Transparent -size #{bounds.width}x#{bounds.height} canvas:white #{file.path}")
-      # system("convert -size #{bounds.width}x#{bounds.height} canvas:white #{file.path}")
-      system("convert trans.png -alpha set -channel a -evaluate set 0%+channel #{file.path}")
+      system("convert -size #{bounds.width}x#{bounds.height} canvas:white #{file.path}")
+      # system("convert trans.png -alpha set -channel a -evaluate set 0%+channel #{file.path}")
       # system("convert #{file.path} -transparent black NikeProd.png")
       # system("convert -size #{bounds.width}x#{bounds.height} -alpha transparent #{file.path}")
       # system("convert #{file.path} -alpha transparent #{file.path}")
@@ -30,11 +30,7 @@ module Heatmap
       # Apply a default 50% opacity
       system("mogrify -channel A -fx \"A*0.50\" #{file.path}")
 
-      file.close
-      system("convert #{file.path} -alpha set -channel K -evaluate set 15%+channel please_work2.png")
-      system("convert #{file.path} -alpha set -channel K -evaluate set 50%+channel please_work3.png") 
-      system("convert #{file.path} -alpha set -channel K -evaluate set 75%+channel please_work4.png") 
-      system("convert #{file.path} -alpha set -channel K -evaluate set 95%+channel please_work5.png")  
+      file.close 
     end
 
   end
