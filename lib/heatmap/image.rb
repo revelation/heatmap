52c9bcc3a1564ce0f9a9a179a775cae1c523f402
Josh Paul <joshua.paul@revelationglobal.com>
2014-10-02 10:56:54 -0700

    adafadsfadsf

--- lib/heatmap/image.rb
+++ lib/heatmap/image.rb
@@ -12,8 +12,8 @@ module Heatmap
 
       # Creating a blank canvas
       # system("convert -alpha Transparent -size #{bounds.width}x#{bounds.height} canvas:white #{file.path}")
-       system("convert -size #{bounds.width}x#{bounds.height} canvas:white #{file.path}")
-      # system("convert kitty.png #{file.path}")
+      # system("convert -size #{bounds.width}x#{bounds.height} pattern:gray100 #{file.path}")
+      system("convert trans.png #{file.path}")
       # system("convert #{file.path} -transparent black NikeProd.png")
       # system("convert -size #{bounds.width}x#{bounds.height} -alpha transparent #{file.path}")
       # system("convert #{file.path} -alpha transparent #{file.path}")
