73a4eebfd80061737f7ded8747fead2c478fd6aa
Josh Paul <joshua.paul@revelationglobal.com>
2014-10-03 18:29:18 -0700

    flipped back gradient

--- lib/heatmap/image.rb
+++ lib/heatmap/image.rb
@@ -8,7 +8,7 @@ module Heatmap
       raise(ArgumentError, "Area needs to be an array of Heatmap::Area objects") if area.empty?
 
       file   = File.new(file, 'w')
-      bounds = Heatmap::Geometry.bounds(area, 100)
+      bounds = Heatmap::Geometry.bounds(area, 25)
 
       # Creating a blank canvas
       # system("convert -alpha Transparent -size #{bounds.width}x#{bounds.height} canvas:white #{file.path}")
