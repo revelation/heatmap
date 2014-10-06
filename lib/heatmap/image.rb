03f1dae89f0fe492c3b57c493af50b0b6f759442
Josh Paul <joshua.paul@revelationglobal.com>
2014-10-06 00:39:50 -0700

    bilinear

--- lib/heatmap/image.rb
+++ lib/heatmap/image.rb
@@ -27,7 +27,7 @@ module Heatmap
       system(compose * ' ')
 
       # Applying color with a LUT
-      system("convert -channel ALL -interpolate Average -clut #{file.path} #{GRADIENT.path} #{file.path}")
+      system("convert -channel ALL -interpolate BiLinear -clut #{file.path} #{GRADIENT.path} #{file.path}")
 
       # Apply a default 50% opacity
       system("mogrify -channel A -fx \"A*0.85\" #{file.path}")
